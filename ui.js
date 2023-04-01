b6f0abc0e6e67112f9b8227d2f63497eee254341
Kevin1358 <tembok511@gmail.com>
2023-04-01 22:29:08 +0700

    implement page select and template

--- ui.js
+++ ui.js
@@ -1,11 +1,35 @@
 class animator{
-    slide;
-    constructor(){
-        this.slide = document.getElementsByClassName("container");
+    page;
+    container;
+    currentPage;
+    constructor(containerId){
+        //Tentukan container halaman
+        this.container = document.getElementById(containerId);
+        //Dapatkan template halaman
+        this.page = document.getElementsByTagName("template");
     }
-    slideSelect(){
-        for (var i = 0; i < this.slide.length; i++) {
-            console.log(this.slideId[i].id); //second console output
+    pageSelect(slideId,pageSpecialFunc){
+        //Dapatkan template
+        try {
+            var child = this.page[slideId].content.cloneNode(true);
+        } catch (error) {
+            //Bila gagal log page not found
+            console.log("Page not found");
+            return false;
+        }
+        if(this.currentPage != undefined){
+            //kalau container tidak kosong, kosongkan dulu
+            this.container.removeChild(this.currentPage);
+        }
+        //Buatkan subcontainer untuk laman
+        var miniContainer = document.createElement("section");
+        miniContainer.className = "container";
+        miniContainer.appendChild(child);
+        //Bila ada special function  jalankan special function
+        this.currentPage = this.container.appendChild(miniContainer);
+        if(pageSpecialFunc != null){
+            pageSpecialFunc();
         }
     }
+    
 }
